32086cd03cce04cdd69b9e92327d6709191dc0ea
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-03-10 16:45:59 +0100

    working browser 🥳

--- src/main/java/org/testshift/testcube/explore/TestCaseExplorer.java
+++ src/main/java/org/testshift/testcube/explore/TestCaseExplorer.java
@@ -1,12 +1,16 @@
 package org.testshift.testcube.explore;
 
+import com.intellij.designer.LightFillLayout;
 import com.intellij.ui.jcef.JBCefApp;
 import com.intellij.ui.jcef.JBCefBrowser;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TestCaseExplorer {
     private JPanel panel;
+    private JScrollPane scroll;
+    private JPanel sp;
 
     public TestCaseExplorer() {
         if (!JBCefApp.isSupported()) {
@@ -15,6 +19,19 @@ public class TestCaseExplorer {
         }
 
         // Use JCEF
-        panel.add(new JBCefBrowser("https://www.jetbrains.com").getComponent());
+        JBCefBrowser browser = new JBCefBrowser("https://www.jetbrains.com");
+        Component component = browser.getComponent();
+        sp.setLayout(new BorderLayout());
+        sp.add(component);
     }
+
+
+    public JPanel getContent() {
+        return panel;
+    }
+
+    public String getDisplayName() {
+        return "Exploring!!!";
+    }
+
 }
