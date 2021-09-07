834cf496aee770663b42c079c85bf6512371fd13
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-09-07 09:43:52 +0200

    update to intellij 2022.2, new junit test detection

--- src/main/java/org/testshift/testcube/amplify/AmplifyTestMarkerContributor.java
+++ src/main/java/org/testshift/testcube/amplify/AmplifyTestMarkerContributor.java
@@ -1,12 +1,14 @@
 package org.testshift.testcube.amplify;
 
+import com.intellij.codeInsight.TestFrameworks;
+import com.intellij.execution.TestStateStorage;
 import com.intellij.execution.lineMarker.RunLineMarkerContributor;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiMethod;
-import com.intellij.testIntegration.TestFailedLineManager;
+import com.intellij.psi.*;
+import com.intellij.psi.util.ClassUtil;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testIntegration.TestFramework;
 import com.intellij.util.Function;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -32,29 +34,43 @@ public class AmplifyTestMarkerContributor extends RunLineMarkerContributor {
 //                }
 //            }
             if (parent instanceof PsiMethod) {
-                TestFailedLineManager.TestInfo testInfo = TestFailedLineManager.getInstance(parent.getProject()).getTestInfo((PsiMethod)parent);
+                /**
+                 * From {@link com.intellij.testIntegration.TestRunLineMarkerProvider#getInfo(PsiElement)}
+                 */
+                PsiClass containingClass = PsiTreeUtil.getParentOfType(parent, PsiClass.class);
+                if (!isTestMethod(containingClass, (PsiMethod)parent)) {
+                    return null;
+                }
                 // test method
                 String testMethodName = ((PsiMethod) parent).getName();
                 String testClassName = Objects.requireNonNull(((PsiMethod) parent).getContainingClass()).getQualifiedName();
 //                if (testClassName == null) {
 //                    return null;
 //                }
-                VirtualFile moduleRoot =
-                        ProjectFileIndex.SERVICE.getInstance(parent.getProject())
-                                                .getContentRootForFile(parent.getContainingFile().getVirtualFile());
-                String moduleRootPath;
-                if (moduleRoot == null) {
-                    moduleRootPath = parent.getProject().getBasePath();
+                VirtualFile file = parent.getContainingFile().getVirtualFile();
+                if (file != null) {
+                    VirtualFile moduleRoot = ProjectFileIndex.SERVICE.getInstance(parent.getProject())
+                                                                     .getContentRootForFile(file);
+                    String moduleRootPath;
+                    if (moduleRoot == null) {
+                        moduleRootPath = parent.getProject().getBasePath();
+                    } else {
+                        moduleRootPath = moduleRoot.getPath();
+                    }
+                    return new Info(TestCubeIcons.AMPLIFY_TEST, tooltipProvider, new StartTestCubeAction(
+                            "Amplify '" + element.getText() + "()'", testClassName, testMethodName, moduleRootPath));
                 }
-                else {
-                    moduleRootPath = moduleRoot.getPath();
-                }
-                return testInfo == null ? null : new Info(TestCubeIcons.AMPLIFY_TEST, tooltipProvider,
-                        new StartTestCubeAction("Amplify '" + element.getText() + "()'", testClassName,
-                                testMethodName, moduleRootPath));
             }
         }
         return null;
     }
 
+    /**
+     * From {@link com.intellij.testIntegration.TestRunLineMarkerProvider#getInfo(PsiElement)}
+     */
+    private static boolean isTestMethod(PsiClass containingClass, PsiMethod method) {
+        if (containingClass == null) return false;
+        TestFramework framework = TestFrameworks.detectFramework(containingClass);
+        return framework != null && framework.isTestMethod(method, false);
+    }
 }
