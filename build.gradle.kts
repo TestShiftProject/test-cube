24b3fd7bdbfbd24e7bb0bd0db1632f17e1b25ab8
Carolin Brandt <laci_noire@live.de>
2024-09-30 09:09:51 +0200

    push to new intellij version and upgrade gradle and java with it

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.intellij.platform.migration") version "2.1.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.0"
     // Gradle Qodana Plugin
@@ -21,24 +22,39 @@ version = properties("pluginVersion")
 // Configure project's dependencies
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+        intellijDependencies()
+    }
 }
 
 dependencies {
     testImplementation("junit:junit:4.12")
     implementation(fileTree(mapOf("dir" to "src/main/resources/dspot", "include" to listOf("*.jar"))))
+    intellijPlatform {
+        create(properties("platformType"), properties("platformVersion"))
+        bundledPlugin("com.intellij.java")
+        instrumentationTools()
+//        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+    }
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
-    downloadSources.set(properties("platformDownloadSources").toBoolean())
-    updateSinceUntilBuild.set(true)
-
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+intellijPlatform {
+    pluginConfiguration {
+        name = properties("pluginName")
+    }
+//    pluginVerification {
+//
+//    }
+//    downloadSources.set(properties("platformDownloadSources").toBoolean())
+//    updateSinceUntilBuild.set(true)
 }
+
+//runPluginVerifier {
+//    ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+//}
+
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
@@ -100,18 +116,14 @@ tasks {
 //        })
 //    }
 
-    runPluginVerifier {
-        ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
-    }
-
     // Configure UI tests plugin
     // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-    }
+//    runIdeForUiTests {
+//        systemProperty("robot-server.port", "8082")
+//        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
+//        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
+//        systemProperty("jb.consents.confirmation.enabled", "false")
+//    }
 
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
